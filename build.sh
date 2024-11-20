d71a3525b4ff2ee8964eadd02d472dd8be69d2cb
YuudaiHiiro <jamroack@gmail.com>
2024-11-20 17:06:27 +0200

    updates

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 # build.sh
+set -o errexit
+
 python -m pip install --upgrade pip
 pip install -r requirements.txt
+
 python manage.py collectstatic --noinput
+python manage.py migrate
+if [[ $CREATE_SUPERUSER ]]
+then
+    python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+fi
