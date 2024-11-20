387c9a386659320e6972f9278f12c821f875df4e
YuudaiHiiro <jamroack@gmail.com>
2024-11-20 18:12:14 +0200

    Add Playwright browser installation to build script

--- build.sh
+++ build.sh
@@ -5,9 +5,14 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-# Install Playwright and dependencies
-PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/src/.playwright playwright install chromium --with-deps
-playwright install-deps
+# Install Playwright with xvfb for Render
+pip install playwright
+pip install pytest-playwright
+playwright install chromium
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+apt-get update
+apt-get install -y google-chrome-stable
 
 python manage.py collectstatic --noinput
 python manage.py migrate
