8714aaee61291c62ca52e7ccf903716d81f304c1
YuudaiHiiro <jamroack@gmail.com>
2024-11-20 21:18:46 +0200

    Add Playwright browser installation to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# build.sh
 set -o errexit
 
 # Install missing libraries
@@ -16,15 +15,9 @@ apt-get update && apt-get install -y \
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-# Install Playwright and browsers
-PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/src/.playwright playwright install
+# Install Playwright and its browsers
 python -m playwright install
 
 # Collect static files and run migrations
 python manage.py collectstatic --noinput
 python manage.py migrate
-
-# Create superuser if the environment variable is set
-if [[ $CREATE_SUPERUSER ]]; then
-    python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
-fi
