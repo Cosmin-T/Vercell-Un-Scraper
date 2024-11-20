188de6654345f5b2c96fcb705aac0fa9a84b9d1d
YuudaiHiiro <jamroack@gmail.com>
2024-11-20 17:24:43 +0200

    Add Playwright browser installation to build script

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
+# Install Playwright browsers
+playwright install chromium
+python -m playwright install
+
 python manage.py collectstatic --noinput
 python manage.py migrate
 if [[ $CREATE_SUPERUSER ]]
