d06ec7828a86455f455e8cadde632fc99029527c
YuudaiHiiro <jamroack@gmail.com>
2024-11-20 17:35:00 +0200

    Add Playwright browser installation to build script

--- build.sh
+++ build.sh
@@ -2,27 +2,12 @@
 # build.sh
 set -o errexit
 
-# Install system dependencies for Playwright
-apt-get update
-apt-get install -y libgstreamer1.0-0 \
-    libgstreamer-plugins-base1.0-0 \
-    libgstreamer-plugins-bad1.0-0 \
-    libenchant-2-2 \
-    libsecret-1-0 \
-    libmanette-0.2-0 \
-    libgles2 \
-    libegl1 \
-    libopus0 \
-    libwebp7 \
-    libwebpdemux2 \
-    libwoff1
-
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-# Install Playwright and browsers
-playwright install chromium
-python -m playwright install
+# Install Playwright with system dependencies
+PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/src/.playwright pip install playwright
+PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/src/.playwright playwright install chromium
 
 python manage.py collectstatic --noinput
 python manage.py migrate
